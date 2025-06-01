239f69117c97a793bfad8ffca18fb3f7713bee64
Rade <radedozet@gmail.com>
2025-06-01 12:24:07 +0200

    Join strings

--- 05_joinStrings/joinStrings-example.js
+++ 05_joinStrings/joinStrings-example.js
@@ -3,6 +3,7 @@ const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
 
+// Hello! My name is Carlos Stevenson and I am 18 years old.
 const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
 console.log(greeting);
