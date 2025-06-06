fcf67e79dc78c6d05d0f781812492dbecb9da163
Rade <radedozet@gmail.com>
2025-06-06 09:43:03 +0200

    Finish exercises 9 - 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, step) {
+    if (start < 1 || start % 1 !== 0 || typeof (step) !== 'number') {
+        return 'ERROR'
+    } else {
+        let result = 0;
+        for (let i = step; i > 0; i--) {
+            result += start;
+            start += 1;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// 7, 4 -> 7 + 8 + 9 + 10
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(temp) {
+  const result = ((9 / 5) * temp + 32);
+  if (result % 1 !== 0) {
+    return Number(result.toFixed(1));
+  } else {
+    return result;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  const result = ((5 / 9) * (temp - 32));
+  if (result % 1 !== 0){
+    return Number(result.toFixed(1));
+  } else {
+    return result;
+  }
 };
 
 // Do not edit below this line
