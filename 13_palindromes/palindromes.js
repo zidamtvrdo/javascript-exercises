e796413f8c8fa2f837f8f972d8d93ab88e609f09
Rade <radedozet@gmail.com>
2025-06-11 16:15:51 +0200

    Make the function check for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const checkString = string
+                        .toLowerCase()
+                        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s+]/g,"");
+
+    const reversedString = string
+                            .toLowerCase()
+                            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s+]/g,"")
+                            .split('')
+                            .reverse()
+                            .join('');
+
+    return checkString === reversedString; 
 };
 
 // Do not edit below this line
