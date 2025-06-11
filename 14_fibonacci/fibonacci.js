5b8951081d885a49f8431ae3f309e70aa33f4ddb
Rade <radedozet@gmail.com>
2025-06-11 18:04:47 +0200

    Make function that calculate certain number in fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const fibList = [0, 1];
+    n = Number(n);
+    if (n < 0 || isNaN(n)) {
+        return 'OOPS'
+    } 
+    for (let i = 1; i < n; i++) {
+        fibList.push(fibList[i] + fibList[i - 1]);
+    }
+    return fibList[n]
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
